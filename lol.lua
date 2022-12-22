9eafc9496eddb5f2122039d15d1ab28a5fbad9ce
difficuItly <110429177+difficuItly@users.noreply.github.com>
2022-12-22 12:46:58 -0600

    Update lol.lua

--- lol.lua
+++ lol.lua
@@ -1,3 +1,34 @@
+local LocalPlayer = game:GetService('Players').LocalPlayer
+
+local FakeCFrameTypes = {}
+local FakeCFrameHook  = nil
+
+local FakeCFramePosX   = 75634
+local FakeCFramePosY   = 45967
+local FakeCFramePosZ   = 34895
+local FakeCframePosR   = 0
+
+game:GetService("RunService").heartbeat:Connect(function()
+    if getgenv().AntiTriggerBot then
+        FakeCFrameTypes[1] = LocalPlayer.Character.HumanoidRootPart.CFrame
+        FakeCFrameTypes[2] = LocalPlayer.Character.HumanoidRootPart.AssemblyLinearVelocity
+    
+        local RootPartCframe = LocalPlayer.Character.HumanoidRootPart.CFrame
+    
+        RootPartCframe = RootPartCframe * CFrame.new(Vector3.new(0, 0, 0))
+        RootPartCframe = RootPartCframe * CFrame.Angles(math.rad(RandomNumberRange(FakeCframePosR)), math.rad(RandomNumberRange(FakeCframePosR)), math.rad(RandomNumberRange(FakeCframePosR)))
+    
+        LocalPlayer.Character.HumanoidRootPart.CFrame = RootPartCframe
+    
+        LocalPlayer.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.new(FakeCFramePosX, FakeCFramePosY, FakeCFramePosZ)
+    
+        game:GetService("RunService").RenderStepped:Wait()
+    
+        LocalPlayer.Character.HumanoidRootPart.CFrame = FakeCFrameTypes[1]
+        LocalPlayer.Character.HumanoidRootPart.AssemblyLinearVelocity = FakeCFrameTypes[2]
+    end
+end
+
 FakeCFrameHook = hookmetamethod(game, '__index', newcclosure(function(Self, Key)
 	if getgenv().AntiTriggerBot then
 		if not checkcaller() then
